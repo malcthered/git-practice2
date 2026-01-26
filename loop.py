cfc98b077a65bdddcdca032145f38ba33b5ca885
malcthered <malcolm.fairs22@gmail.com>
2026-01-26 09:20:12 +0000

    python practice

---
+++ loop.py
@@ -0,0 +1,5 @@
+x = 0
+for i in range(1, 6):
+    if i % 2 == 0:
+        x += i
+print(x)
---
+++ loop2.py
@@ -0,0 +1,6 @@
+languages = ["Python", "Java", "C"]
+for language in languages:
+    if language == "Python":
+        print(language.upper())
+    else:
+        print(language.title())

f87f4207937d7fa2e8d62825fae19a20bd35af2e
malcthered <malcolm.fairs22@gmail.com>
2026-01-28 13:58:35 +0000

    Initial commits

---
+++ arr.py
@@ -0,0 +1,6 @@
+arr = [10,20,30,40,50]
+
+arr2 = [x *2 for x in arr]
+
+print(arr * 2)
+print(arr2)
---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 6;
+    int b = 8;
+    cout << a + b ;
+
+    return 0;
+}

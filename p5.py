c775f9b5cb390bd9831c37260440eb65f58a91ab
malcthered <malcolm.fairs22@gmail.com>
2026-01-29 15:13:21 +0000

    Add functionality to find common letters between words

---
+++ p5.py
@@ -0,0 +1,9 @@
+strs = ["flower", "flow", "flight"]
+strs.sort()
+result = ""
+for i in range(len(strs[0])):
+    if strs[0][i] == strs[-1][i]:
+        result += strs[0][i]
+    else:
+        break
+print(result)  # Output: "fl"

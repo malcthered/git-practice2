8caa73c2cec0e4a7041f7e52c760beb66c65ee15
malcthered <malcolm.fairs22@gmail.com>
2026-02-17 11:53:46 +0000

    python sort example

---
+++ p11.py
@@ -0,0 +1,7 @@
+nums = list(range(1, 6, 2)) # 1, 3, 5
+print(nums)  #
+print(sum(nums )) # 
+
+nums2 = list(range(1, 12, 3)) # 1, 4, 7, 10
+print(nums2)  #
+print(sum(nums2)) # 22  
---
+++ p13.py
@@ -0,0 +1,4 @@
+lst = [1, 2, 3]   #
+for i in range(len(lst)):
+    lst[i] = lst[i] * i   
+print(lst)  

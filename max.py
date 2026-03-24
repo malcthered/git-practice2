d4691b352bc88b61a8a0970c1bee616dfad34c88
malcthered <malcolm.fairs22@gmail.com>
2026-03-24 11:43:33 +0000

    Add min/max value in python

---
+++ max.py
@@ -0,0 +1,5 @@
+data = [10, 20, 30, 40, 50]
+max_value = max(data)
+min_value = min(data)
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)
--- p32.py
+++ p32.py
@@ -1,4 +1,4 @@
 x = [10, 20, 30]   # a list of integers
 x.append((40, 50)) # appending a tuple to the list counts as one element
 print(x)           # printing the updated list
-print(len(x))      # printing the length of the updated list
+print(len(x))      # printing the length of the updated list - 4 as the tuple is counted as one element
